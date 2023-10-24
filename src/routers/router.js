46d4e80f1cf1c6903c036fb608cc600ddf8181a3
Deepak Porwal <deepak.porwal@impelsys.com>
2023-10-24 13:07:56 +0530

    Added error page if requested page didn't find

--- src/routers/router.js
+++ src/routers/router.js
@@ -3,11 +3,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from '../pages/Home';
 import ProductPage from '../pages/Products';
 import RootLayout from '../pages/RootLayout';
+import ErrorPage from '../pages/Error';
 
 // Create router as JS object, this will latest which work React version > 6.4
 const router = createBrowserRouter(
     [
-      {path: '', element: <RootLayout/>, 
+      {path: '', element: <RootLayout/>,  errorElement: <ErrorPage/>,
       children: [
         {path: '', element: <HomePage/>}, //define as jsx element or compnent name
         {path: '/product', Component: ProductPage},
