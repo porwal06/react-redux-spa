7df066d2a85220b5f05713833b9d9711eda925c0
Deepak Porwal <deepak.porwal@impelsys.com>
2023-10-24 13:51:31 +0530

    Added dynamic route & get dynamic value

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,6 +4,7 @@ import HomePage from '../pages/Home';
 import ProductPage from '../pages/Products';
 import RootLayout from '../pages/RootLayout';
 import ErrorPage from '../pages/Error';
+import ProductDetailPage from '../pages/ProductDetail';
 
 // Create router as JS object, this will latest which work React version > 6.4
 const router = createBrowserRouter(
@@ -12,6 +13,7 @@ const router = createBrowserRouter(
       children: [
         {path: '', element: <HomePage/>}, //define as jsx element or compnent name
         {path: '/product', Component: ProductPage},
+        {path: '/product/:productId', element: <ProductDetailPage />}
       ]},
       
     ]
